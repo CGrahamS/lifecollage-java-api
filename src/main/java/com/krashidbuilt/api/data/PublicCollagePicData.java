7b767a7efa04b9c1221766f67d848ef613dba21b
Caleb Stevenson <cgrahamstevenson@gmail.com>
2017-02-17 12:17:29 -0800

    add PublicCollagePicData class and all collage pics of a specific collage endpoint

---
+++ src/main/java/com/krashidbuilt/api/data/PublicCollagePicData.java
@@ -0,0 +1,46 @@
+package com.krashidbuilt.api.data;
+
+import com.krashidbuilt.api.helpers.ObjectMapper;
+import com.krashidbuilt.api.model.CollagePic;
+import com.krashidbuilt.api.service.MySQL;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by CGrahamS on 2/17/17.
+ */
+
+@SuppressFBWarnings("OBL_UNSATISFIED_OBLIGATION")
+public class PublicCollagePicData {
+    private static Logger logger = LogManager.getLogger();
+
+    public static List<CollagePic> getCollagePics(int collageId) {
+        logger.debug("GET ALL PICTURES WITH COLLAGE ID {} START");
+        List<CollagePic> pictures = new ArrayList<>();
+
+        MySQL db = new MySQL();
+
+        String sql = "SELECT * FROM picture WHERE collage_id =?";
+
+        try {
+            db.setpStmt(db.getConn().prepareStatement(sql));
+
+            db.getpStmt().setInt(1, collageId);
+
+            db.setRs(db.getpStmt().executeQuery());
+            while (db.getRs().next()) {
+                pictures.add(ObjectMapper.collagePic(db.getRs()));
+            }
+        } catch (SQLException e) {
+            logger.error("UNABLE TO FIND PICTURES",  e);
+        }
+
+        logger.debug("GET ALL PICTURES WITH COLLAGE ID {} END", collageId);
+        return pictures;
+    }
+}
--- src/main/java/com/krashidbuilt/api/resource/PublicCollagePicResource.java
+++ src/main/java/com/krashidbuilt/api/resource/PublicCollagePicResource.java
@@ -2,6 +2,7 @@ package com.krashidbuilt.api.resource;
 
 import com.krashidbuilt.api.data.CollageData;
 import com.krashidbuilt.api.data.CollagePicData;
+import com.krashidbuilt.api.data.PublicCollagePicData;
 import com.krashidbuilt.api.model.Collage;
 import com.krashidbuilt.api.model.CollagePic;
 import com.krashidbuilt.api.model.Error;
@@ -14,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by CGrahamS on 2/17/17.
@@ -23,6 +25,37 @@ import javax.ws.rs.core.Response;
 @Api(value = "public/picture", description = "Interact with a public picture object", tags = "picture")
 public class PublicCollagePicResource {
     private static Logger logger = LogManager.getLogger();
+
+    @GET()
+    @Path("all/collageId/{collageId}")
+    @Produces("application/json")
+    @ApiOperation(value = "Retrieve all pictures associated with collage",
+            notes = "Returns all pictures associated with the collage that matches the supplied id",
+            response = CollagePic.class,
+            responseContainer = "List"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Collage not found", response = Error.class)
+    })
+    @Consumes("application/json")
+    public Response getCollagePictures(@PathParam("collageId") int collageId) {
+        logger.debug("Find most recent picture of collage with id {} at PublicCollagePicResource", collageId);
+
+        Collage collage = CollageData.getCollage(collageId);
+        List<CollagePic> out;
+
+        if (!collage.isValid()) {
+            logger.debug("CANNOT FIND COLLAGE");
+            Error error = Error.notFound("Collage", collageId);
+            return Response.status(error.getStatusCode()).entity(error).build();
+        }
+
+        out = PublicCollagePicData.getCollagePics(collageId);
+
+        // return most recent picture of collage with supplied id
+        logger.debug("Get single picture" + out.toString());
+        return Response.ok(out).build();
+    }
 
     @GET()
     @Path("collageId/{collageId}")
